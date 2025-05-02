6f02a734822e8120ddca4573fe1e4845e59a1b5b
abdulkarim.salem <abdullarimsalem@gmail.com>
2025-05-02 17:45:31 +0300

    crud request

---
+++ lib/core/class/crud.dart
@@ -0,0 +1,26 @@
+import 'dart:convert';
+
+import 'package:dartz/dartz.dart';
+import 'package:ecommerce_app/core/class/status_request.dart';
+import 'package:ecommerce_app/core/functions/check_internet.dart';
+import 'package:http/http.dart' as http;
+
+class Crud {
+  Future<Either<StatusRequest, Map>> postData(String linkUrl, Map data) async {
+    try {
+      if (await checkInternet()) {
+        var response = await http.post(Uri.parse(linkUrl), body: data);
+        if (response.statusCode == 200 || response.statusCode == 201) {
+          Map responseBody = jsonDecode(response.body);
+          return Right(responseBody);
+        } else {
+          return Left(StatusRequest.serverFailure);
+        }
+      } else {
+        return Left(StatusRequest.offlineFailure);
+      }
+    } catch (_) {
+      return Left(StatusRequest.serverFailure);
+    }
+  }
+}
--- lib/core/functions/check_internet.dart
+++ lib/core/functions/check_internet.dart
@@ -1,12 +1,14 @@
 import 'dart:io';
 
-checkInternet() async {
+Future<bool> checkInternet() async {
   try {
     List<InternetAddress> result = await InternetAddress.lookup("google.com");
     if (result.isEmpty && result[0].rawAddress.isNotEmpty) {
       return true;
+    }else {
+      return false;
     }
-  }on SocketException catch (_) {
-    return true;
+  } on SocketException catch (_) {
+    return false;
   }
 }
